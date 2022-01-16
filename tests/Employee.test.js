28124042c9bc7acee54388aafa7e1ff2022c4765
Shy <fleetwoodshybreasia@gmail.com>
2022-01-16 16:08:57 -0500

    Employee.js created

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,1 +1,12 @@
-const employee = requirer('./lib/Employee.js');
+const { it } = require("jest-circus");
+const { describe } = require("yargs");
+
+const Employee = requirer('./lib/Employee.js');
+
+describe('employee', () => {
+    describe('Initialization', () => {
+        it('should create an object with a name, id, email and role if provided valid arguments', () => {
+            const employee = new Employee(Tilvia,)
+        })
+    })
+})
