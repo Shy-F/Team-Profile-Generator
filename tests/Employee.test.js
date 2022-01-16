70b3562edbab6e849f21ac29d677cfbde34d2845
Shy <fleetwoodshybreasia@gmail.com>
2022-01-16 16:58:16 -0500

    Employee.test.js created

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,3 +1,4 @@
+const { expect } = require("@jest/globals");
 const { it } = require("jest-circus");
 const { describe } = require("yargs");
 
@@ -6,7 +7,30 @@ const Employee = requirer('./lib/Employee.js');
 describe('employee', () => {
     describe('Initialization', () => {
         it('should create an object with a name, id, email and role if provided valid arguments', () => {
-            const employee = new Employee(Tilvia,)
-        })
-    })
-})
+            const employee = new Employee('Tilvia, til.newman@gmail.com, 890');
+            expect(employee.name).toEqual(expect.any(String));
+            expect(employee.email).toEqual(expect.any(String));
+            expect(employee.id).toEqual(expect.any(Number));
+        });
+
+        it('Get name from constructor', () => {
+            const employee = new Employee('Tilvia, til.newman@gmail.com, 890');
+            expect(employee.getName()).toEqual(expect.any(String));
+        });
+
+        it('Get email from constructor', () => {
+            const employee = new Employee('Tilvia, til.newman@gmail.com, 890');
+            expect(employee.getEmail()).toEqual(expect.any(String));
+        });
+
+        it('Get id from cnstructor', () => {
+            const employee = new Employee('Tilvia, til.newman@gmail.com, 890');
+            expect(employee.getId()).toEqual(expect.any(Number));
+        });
+
+        it('Get rol from constructor', () => {
+            const employee = new Employee('Tilvia, til.newman@gmail.com, 890');
+            expect(employee.getRole()).toEqual('Employee');
+        });
+    });
+});
